504789f5889afd7022cea2991a94c9df85430e45
Terminator1601 <rishisharma4201@gmail.com>
2023-12-14 16:33:04 +0530

    optimized navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,42 +1,30 @@
-// import stylesheet from
-
 import React from "react";
 
+const NavItem = ({ icon, text }) => (
+  <div className="grid grid-cols-2 py-2 px-1 flex-auto items-center">
+    <a href="">
+      <img src={icon} alt={text} className="w-8" />
+      {text}
+    </a>
+  </div>
+);
+
 const Navbar = () => {
+  const navItems = [
+    { icon: "path-to-icon", text: "Hotels" },
+    { icon: "path-to-icon", text: "Restaurants" },
+    { icon: "path-to-icon", text: "Spa" },
+    { icon: "path-to-icon", text: "Gym" },
+    { icon: "path-to-icon", text: "Consultant" },
+    { icon: "path-to-icon", text: "Coaching" },
+  ];
+
   return (
-    <>
-      <div className=" grid grid-cols-6 flex-auto justify-center text-center">
-        <div className="grid grid-cols-2 py-0 flex-auto">
-          <span className="material-symbols-outlined"></span>
-          <a href="">Hotels</a>
-        </div>
-        <div className="grid grid-cols-2 py-2 px-1 flex-auto">
-          <img src="" alt="1" srcset="" className="w-8" />
-
-          <a href="">Restaurants</a>
-        </div>
-        <div className="grid grid-cols-2 py-2 px-1 flex-auto">
-          <img src="" alt="1" srcset="" className="w-8" />
-
-          <a href="">Spa</a>
-        </div>
-        <div className="grid grid-cols-2 py-2 px-1 flex-auto">
-          <img src="" alt="1" srcset="" className="w-8" />
-
-          <a href="">Gym</a>
-        </div>
-        <div className="grid grid-cols-2 py-2 px-1 flex-auto">
-          <img src="" alt="1" srcset="" className="w-8" />
-
-          <a href="">Consultant</a>
-        </div>
-        <div className="grid grid-cols-2 py-2 px-1 flex-auto">
-          <img src="" alt="1" srcset="" className="w-8" />
-
-          <a href="">Coaching</a>
-        </div>
-      </div>
-    </>
+    <div className="grid grid-cols-6 flex-auto justify-center text-center">
+      {navItems.map((item, index) => (
+        <NavItem key={index} {...item} />
+      ))}
+    </div>
   );
 };
 
