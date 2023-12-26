c8e3b08eb18e1d8553b1ddbbcf5c611ac38c1c9e
Terminator1601 <rishisharma4201@gmail.com>
2023-12-26 22:29:46 +0530

    fixed some more error 12

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
-"use client"
+"use client";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 import PopularSearches from "../components/PopularSearches/PopularSearches";
 import RecentlyAdded from "../components/RecentlyAdded/RecentlyAdded";
 import FloatingWidgets from "../components/floatingWidget/FloatingWidget";
-import UserLocationPopup from "../components/UserLocationPopup/UserLocationPopup";
-import Image from "next/image";
-import dynamic from "next/dynamic";
 import Footer from "../components/Footer/Footer";
 import Services from "../components/Services/Services";
 import Intro from "../components/Introduction/Intro";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Home() {
   return (
-    <BrowserRouter>
+    <>
       <main className="sm:mx-20 md:px-16 md:py-0 lg:px-16 lg:py-0  bg-white">
         <Header />
         <hr />
@@ -28,6 +25,6 @@ export default function Home() {
         <Services />
       </main>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
