37a0e652640eeb31df2a6ea577b9045be436c6dc
Terminator1601 <rishisharma4201@gmail.com>
2023-12-27 13:50:10 +0530

    added logic to the database table

--- components/Admin/PendingUpdate/ApprovedCards.tsx
+++ components/Admin/PendingUpdate/ApprovedCards.tsx
@@ -1,7 +1,13 @@
-// cardsForAdmin.tsx
+// Import necessary dependencies
 import React, { useEffect, useState } from "react";
 import { db } from "../../../database/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 interface Listing {
   id: string;
@@ -12,36 +18,72 @@ interface Listing {
   // Add other properties as needed
 }
 
-interface CardsForAdminProps {
+interface ApprovedCardsProps {
   // Define any props needed by your component
 }
+// ... (existing imports)
 
-const ApprovedCards: React.FC<CardsForAdminProps> = () => {
-  const [ApprovedListings, setApprovedListings] = useState<Listing[]>([]);
+const ApprovedCards: React.FC<ApprovedCardsProps> = () => {
+  const [approvedListings, setApprovedListings] = useState<Listing[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const ApprovedListingsCollection = collection(db, "ApprovedListing");
-        const snapshot = await getDocs(ApprovedListingsCollection);
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        })) as Listing[];
+    const approvedListingsCollection = collection(db, "ApprovedListing");
 
-        setApprovedListings(data);
-      } catch (error) {
-        console.error("Error fetching data from Firestore: ", error);
-      }
-    };
+    const unsubscribe = onSnapshot(approvedListingsCollection, (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Listing[];
 
-    fetchData();
+      setApprovedListings(data);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
   }, []); // Run this effect only once when the component mounts
+
+  const handleEdit = async (listing: Listing) => {
+    try {
+      const freeListingsCollection = collection(db, "FreeListing");
+      await addDoc(freeListingsCollection, listing);
+
+      const approvedListingsCollection = collection(db, "ApprovedListing");
+      await deleteDoc(doc(approvedListingsCollection, listing.id));
+
+      setApprovedListings((prevListings) =>
+        prevListings.filter((prevListing) => prevListing.id !== listing.id)
+      );
+
+      window.alert(
+        "Listing edited successfully and moved to FreeListing table."
+      );
+    } catch (error) {
+      console.error("Error editing listing: ", error);
+    }
+  };
+
+  const handleDisapprove = async (listing: Listing) => {
+    try {
+      const approvedListingsCollection = collection(db, "ApprovedListing");
+      await deleteDoc(doc(approvedListingsCollection, listing.id));
+
+      setApprovedListings((prevListings) =>
+        prevListings.filter((prevListing) => prevListing.id !== listing.id)
+      );
+
+      window.alert("Listing disapproved successfully.");
+    } catch (error) {
+      console.error("Error disapproving listing: ", error);
+    }
+  };
 
   return (
     <>
-      {ApprovedListings.map((listing) => (
-        <div key={listing.id} className="grid grid-cols-5 gap-3 p-2">
+      {approvedListings.map((listing, index) => (
+        <div
+          key={`${listing.id}-${index}`}
+          className="grid grid-cols-5 gap-3 p-2"
+        >
           <div>
             <img src={listing.shopImage} alt={listing.shopImage} />
           </div>
@@ -52,10 +94,14 @@ const ApprovedCards: React.FC<CardsForAdminProps> = () => {
           </div>
           <div className="grid grid-rows-2 ">
             <div>
-              <button type="button">Dis-Approve</button>
+              <button type="button" onClick={() => handleDisapprove(listing)}>
+                Dis-Approve
+              </button>
             </div>
             <div>
-              <button type="button">Edit</button>
+              <button type="button" onClick={() => handleEdit(listing)}>
+                Edit
+              </button>
             </div>
           </div>
         </div>
--- components/Admin/PendingUpdate/cardsForAdmin.tsx
+++ components/Admin/PendingUpdate/cardsForAdmin.tsx
@@ -1,8 +1,15 @@
-// cardsForAdmin.tsx
+// Import necessary dependencies
 import React, { useEffect, useState } from "react";
 import { db } from "../../../database/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
 
+// Define the Listing interface
 interface Listing {
   id: string;
   name?: string;
@@ -12,6 +19,7 @@ interface Listing {
   // Add other properties as needed
 }
 
+// Define any props needed by your component
 interface CardsForAdminProps {
   // Define any props needed by your component
 }
@@ -20,23 +28,44 @@ const CardsForAdmin: React.FC<CardsForAdminProps> = () => {
   const [freeListings, setFreeListings] = useState<Listing[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const freeListingsCollection = collection(db, "FreeListing");
-        const snapshot = await getDocs(freeListingsCollection);
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        })) as Listing[];
+    const freeListingsCollection = collection(db, "FreeListing");
 
-        setFreeListings(data);
-      } catch (error) {
-        console.error("Error fetching data from Firestore: ", error);
-      }
-    };
+    // Set up a real-time listener for the FreeListing collection
+    const unsubscribe = onSnapshot(freeListingsCollection, (snapshot) => {
+      console.log("FreeListing collection updated:", snapshot.docs.length);
 
-    fetchData();
-  }, []); // Run this effect only once when the component mounts
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Listing[];
+
+      setFreeListings(data);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  const approveListing = async (listing: Listing) => {
+    try {
+      console.log("Deleting from FreeListing:", listing.id);
+      const freeListingsCollection = collection(db, "FreeListing");
+      await deleteDoc(doc(freeListingsCollection, listing.id));
+
+      console.log("Adding to ApprovedListing:", listing);
+      const approvedListingsCollection = collection(db, "ApprovedListing");
+      await addDoc(approvedListingsCollection, listing);
+
+      console.log("Deletion and approval successful.");
+
+      // Notify the user with a window.alert
+      window.alert("Listing approved successfully!");
+    } catch (error) {
+      console.error("Error approving listing: ", error);
+      // Notify the user with a window.alert in case of an error
+      window.alert("Error approving listing. Please try again.");
+    }
+  };
 
   return (
     <>
@@ -50,9 +79,17 @@ const CardsForAdmin: React.FC<CardsForAdminProps> = () => {
             <p>{listing.description}</p>
             <hr />
           </div>
-          <div className="grid grid-rows-2 ">
+          <div className="grid grid-rows-2">
             <div>
-              <button type="button">Approve</button>
+              <button
+                type="button"
+                onClick={() => {
+                  approveListing(listing);
+                  // No need to manually update state; it will be handled by the real-time listener
+                }}
+              >
+                Approve
+              </button>
             </div>
             <div>
               <button type="button">Edit</button>
