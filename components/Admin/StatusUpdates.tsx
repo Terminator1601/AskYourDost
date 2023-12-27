ada22238f29b51a62e5cb0fdc381093c7d06bb2d
Terminator1601 <rishisharma4201@gmail.com>
2023-12-27 11:18:08 +0530

    added data of table from the database to the component

--- components/Admin/StatusUpdates.tsx
+++ components/Admin/StatusUpdates.tsx
@@ -1,20 +1,24 @@
-
-
+// StatusUpdates.tsx
 import React from "react";
 import { db } from "../../database/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
+import CardsForAdmin from "../Admin/cardsForAdmin";
 
 interface StatusUpdatesProps {
-  pendingUpdatesCount: number;
+  pendingUpdatesCount?: number;
 }
 
-const StatusUpdates: React.FC<StatusUpdatesProps> = ({ pendingUpdatesCount }) => {
+const StatusUpdates: React.FC<StatusUpdatesProps> = ({
+  pendingUpdatesCount,
+}) => {
   return (
     <>
       <div>StatusUpdates</div>
       <div className="grid grid-cols-3 text-center">
         <div>Number of pending Updates: {pendingUpdatesCount}</div>
-        <div>hello</div>
+        <div>
+          <CardsForAdmin />
+        </div>
         <div>hello</div>
       </div>
     </>
@@ -25,7 +29,7 @@ export const getServerSideProps = async () => {
   try {
     const freeListingsCollection = collection(db, "FreeListing");
     const snapshot = await getDocs(freeListingsCollection);
-    const count = snapshot.size; // Get the number of documents in the collection
+    const count = snapshot.size;
 
     return {
       props: {
---
+++ components/Admin/cardsForAdmin.tsx
@@ -0,0 +1,62 @@
+// cardsForAdmin.tsx
+import React, { useEffect, useState } from "react";
+import { db } from "../../database/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+interface Listing {
+  id: string;
+  name?: string;
+  shopImage?: string;
+  description?: string;
+  services?: string;
+  // Add other properties as needed
+}
+
+interface CardsForAdminProps {
+  // Define any props needed by your component
+}
+
+const CardsForAdmin: React.FC<CardsForAdminProps> = () => {
+  const [freeListings, setFreeListings] = useState<Listing[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const freeListingsCollection = collection(db, "FreeListing");
+        const snapshot = await getDocs(freeListingsCollection);
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Listing[];
+
+        setFreeListings(data);
+      } catch (error) {
+        console.error("Error fetching data from Firestore: ", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Run this effect only once when the component mounts
+
+  return (
+    <>
+      {freeListings.map((listing) => (
+        <div key={listing.id} className="grid grid-cols-5 gap-3">
+          <div>
+            <img src={listing.shopImage} alt={listing.shopImage} />
+          </div>
+          <div className="grid col-span-3">
+            <h2>{listing.name || "No Name Available"}</h2>
+            <p>{listing.description}</p>
+          </div>
+          <div>
+            <button type="button">Show Details</button>
+            <button type="button">Contact Us</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CardsForAdmin;
