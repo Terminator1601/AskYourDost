49fd8d23dae12717787eeb8c5c7cd2bb6764fffa
Terminator1601 <rishisharma4201@gmail.com>
2024-01-01 11:33:56 +0530

    added link to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,18 +1,50 @@
+// import React from "react";
+// import { navItems } from "../../app/MainData";
+// import Login from "../Login&Signup/SignIn";
+
+// interface NavItemProps {
+//   icon: string; // Assuming 'icon' is a string, adjust the type accordingly
+//   text: string;
+// }
+
+// const NavItem: React.FC<NavItemProps> = ({ icon, text }) => (
+//   <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 py-2 px-1 flex-auto items-center text-center">
+//     <a href="">
+//       <img src={icon} alt={text} className="w-8" />
+//       {text}
+//     </a>
+//   </div>
+// );
+
+// const Navbar = () => {
+//   return (
+//     <div className="grid grid-cols-6 flex-auto justify-center text-center">
+//       {navItems.map((item, index) => (
+//         <NavItem key={index} {...item} />
+//       ))}
+//       {/* <Login /> */}
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
 import React from "react";
+import Link from "next/link"; // Import Link from next/link
 import { navItems } from "../../app/MainData";
 import Login from "../Login&Signup/SignIn";
 
 interface NavItemProps {
-  icon: string; // Assuming 'icon' is a string, adjust the type accordingly
+  icon: string;
   text: string;
 }
 
 const NavItem: React.FC<NavItemProps> = ({ icon, text }) => (
   <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 py-2 px-1 flex-auto items-center text-center">
-    <a href="">
+    <Link href={`/SearchResults?query=${encodeURIComponent(text)}`}>
       <img src={icon} alt={text} className="w-8" />
       {text}
-    </a>
+    </Link>
   </div>
 );
 
