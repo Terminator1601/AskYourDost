e6806cccd838ba853f59300be559714ee41c648c
Terminator1601 <rishisharma4201@gmail.com>
2024-01-09 12:48:33 +0530

    added styling in introduction section

--- components/Introduction/Intro.tsx
+++ components/Introduction/Intro.tsx
@@ -1,23 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 const Intro = () => {
   return (
-    // <div>Intro</div>
     <>
-      <span style={{ color: "var(--orange)" }}>Ask</span>
-      <span style={{ color: "var(--green)" }}>Your</span>
-      <span style={{ color: "var(--blue)" }}>Dost</span>
-      <span>
-        {" "}
-        is a platform which provides you the information of the products and
-        services which are provided in your locality.
-      </span>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
-        perspiciatis esse, repellat quae obcaecati fuga accusamus cumque
-        asperiores debitis deleniti vero rerum. Accusantium possimus vitae
-        excepturi dignissimos rem perspiciatis animi?
-      </p>
+      <div className="text-center py-36 mx-5">
+        <span style={{ color: "var(--orange)" }}>Ask</span>
+        <span style={{ color: "var(--green)" }}>Your</span>
+        <span style={{ color: "var(--blue)" }}>Dost</span>
+        <span>
+          {" "}
+          is a platform which provides you the information of the products and
+          services which are provided in your locality.
+        </span>
+        <p>
+          Here you can find information about the services available in your
+          locality and you can also list your business services in this platform
+          by simply clicking the{" "}
+          <span className="hover:brightness-150">
+            <a href="/freeListing" style={{ color: "var(--blue)" }}>
+              FREELISTING
+            </a>
+          </span>{" "}
+          button and filling the details of your business.
+        </p>
+      </div>
     </>
   );
 };
