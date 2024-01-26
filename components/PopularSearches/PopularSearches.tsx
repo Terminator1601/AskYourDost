edd8c4675d8f657ebae0283652011e2001af1e7b
Terminator1601 <rishisharma4201@gmail.com>
2024-01-26 22:29:07 +0530

    fixed some error

--- components/PopularSearches/PopularSearches.tsx
+++ components/PopularSearches/PopularSearches.tsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import Cards from "../Cards/HomepageCards";
-// import { popularSectionData } from "../../app/MainData";
-// import "tailwindcss/tailwind.css";
-// const PopularSearches = () => {
-//   return (
-//     <div className="py-5">
-//       <div className="text-lg font-bold pb-5">Popular Searches</div>
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 text-center">
-//         {popularSectionData.map((item, index) => (
-//           <a
-//             href={`/SearchResults?query=${encodeURIComponent(item.title || "")}`}
-//             key={index}
-//           >
-//             <div className="px-3 text-center">
-//               <Cards
-//                 title={item.title}
-//                 imageUrl={item.imageUrl}
-//                 description={item.description}
-//               />
-//             </div>
-//           </a>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PopularSearches;
-
 import React, { useState, useEffect } from "react";
 import Cards from "../Cards/HomepageCards";
 import { popularSectionData } from "../../app/MainData";
