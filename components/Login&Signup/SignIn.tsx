4d78cbbb7e621b98857c9cb3a6bc84495dd98855
Terminator1601 <rishisharma4201@gmail.com>
2024-01-28 19:58:41 +0530

    removed firebase auth

--- components/Login&Signup/SignIn.tsx
+++ components/Login&Signup/SignIn.tsx
@@ -1,22 +1,206 @@
-import { getDocs, query, where } from "firebase/firestore";
+// import { getDocs, query, where, collection } from "firebase/firestore";
+// import React, { useState } from "react";
+// import { db } from "../../database/firebaseConfig";
+// import crypto from "crypto";
+// import { UserProvider } from "@/database/User/UserContext";
+// import { useUser } from "@/database/User/UserContext";
+
+// const SignIn: React.FC = () => {
+//   const { updateUser /* other user data */ } = useUser();
+
+//   const [formData, setFormData] = useState({
+//     email: "",
+//     password: "",
+//     captcha: "",
+//     enteredCaptcha: "",
+//     status: "offline",
+//   });
+
+//   const generateCaptcha = () => {
+//     const captcha = crypto.randomBytes(3).toString("hex");
+//     setFormData({ ...formData, captcha, enteredCaptcha: "" });
+//   };
+
+//   const handleRefreshCaptcha = () => {
+//     generateCaptcha();
+//   };
+
+//   const [isLoginMode, setIsLoginMode] = useState(true);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [username, setUsername] = useState("");
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const handleSuccessfulLogin = (username: string) => {
+//     setIsLoggedIn(true);
+//     setUsername(username);
+//     setFormData((prevData) => ({ ...prevData, status: "online" }));
+//     updateUser({ ...formData });
+//   };
+
+//   const handleSubmit = async () => {
+//     try {
+//       if (formData.captcha !== formData.enteredCaptcha) {
+//         throw new Error("Invalid captcha");
+//       }
+
+//       // Check if the user exists with the provided email
+//       const userQuery = query(
+//         collection(db, "userData"),
+//         where("email", "==", formData.email)
+//       );
+//       const userSnapshot = await getDocs(userQuery);
+
+//       if (userSnapshot.empty) {
+//         throw new Error("Email not registered. Please register.");
+//       }
+
+//       // Check if the password matches
+//       const userData = userSnapshot.docs[0].data();
+//       if (userData.password !== formData.password) {
+//         throw new Error("Invalid password");
+//       }
+
+//       console.log("Login successful!");
+//       window.alert(`Welcome ${userData.username}! Login successful!`);
+//       handleSuccessfulLogin(userData.username);
+//     } catch (error: any) {
+//       console.error(`Error during login:`, error);
+//       window.alert(`Error during login: ${error.message}`);
+//       if (error.message === "Email not registered. Please register.") {
+//         setIsLoginMode(false);
+//       }
+//     } finally {
+//       generateCaptcha();
+//     }
+//   };
+
+//   const toggleMode = () => {
+//     setIsLoginMode((prevMode) => !prevMode);
+//     generateCaptcha();
+//   };
+
+//   React.useEffect(() => {
+//     generateCaptcha();
+//   }, []);
+
+//   return (
+//     <UserProvider>
+//       <div className="max-w-md mx-auto mt-8 p-4 bg-white rounded-md shadow-md">
+//         {isLoggedIn ? (
+//           <>
+//             <p className="text-xl font-semibold mb-4">
+//               Welcome {username}! You are logged in.
+//             </p>
+//             <button
+//               onClick={() => (window.location.href = "/")}
+//               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+//             >
+//               Go to Home Page
+//             </button>
+//           </>
+//         ) : (
+//           <>
+//             <h2 className="text-2xl font-semibold mb-4">
+//               {isLoginMode ? "Login" : "Registration"} Form
+//             </h2>
+//             <label className="block mb-2">
+//               Email:
+//               <input
+//                 type="email"
+//                 name="email"
+//                 value={formData.email}
+//                 onChange={handleChange}
+//                 className="block w-full mt-1 p-2 border rounded-md"
+//               />
+//             </label>
+//             <label className="block mb-2">
+//               Password:
+//               <input
+//                 type="password"
+//                 name="password"
+//                 value={formData.password}
+//                 onChange={handleChange}
+//                 className="block w-full mt-1 p-2 border rounded-md"
+//               />
+//             </label>
+//             <label className="block mb-2">
+//               Captcha: {formData.captcha}
+//               <input
+//                 type="text"
+//                 name="enteredCaptcha"
+//                 value={formData.enteredCaptcha}
+//                 onChange={handleChange}
+//                 className="block w-full mt-1 p-2 border rounded-md"
+//               />
+//             </label>
+//             <div className="flex space-x-4">
+//               <button
+//                 onClick={handleSubmit}
+//                 className={`flex-1 ${
+//                   isLoginMode ? "bg-green-500" : "bg-blue-500"
+//                 } text-white px-4 py-2 rounded-md hover:${
+//                   isLoginMode ? "bg-green-600" : "bg-blue-600"
+//                 }`}
+//               >
+//                 {isLoginMode ? "Login" : "Register"}
+//               </button>
+//               <button
+//                 onClick={handleRefreshCaptcha}
+//                 className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"
+//               >
+//                 Refresh Captcha
+//               </button>
+//             </div>
+//             <p className="mt-2">
+//               {isLoginMode ? "New user? " : "Already have an account? "}
+//               <button
+//                 onClick={toggleMode}
+//                 className="text-blue-500 hover:underline focus:outline-none"
+//               >
+//                 {isLoginMode ? "Register here" : "Login here"}
+//               </button>
+//             </p>
+//           </>
+//         )}
+//       </div>
+//     </UserProvider>
+//   );
+// };
+
+// export default SignIn;
+
+
+
+
+
+
+import { getDocs, query, where, collection } from "firebase/firestore";
 import React, { useState } from "react";
-import { auth, db } from "../../database/firebaseConfig";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
-import { doc, collection, setDoc } from "firebase/firestore";
+import { db } from "../../database/firebaseConfig";
 import crypto from "crypto";
 import { UserProvider } from "@/database/User/UserContext";
 import { useUser } from "@/database/User/UserContext";
 
+type UserData = {
+  username: string;
+  email: string;
+  phone: string;
+  password: string;
+  captcha: string;
+  enteredCaptcha: string;
+  status: string;
+};
 
 const SignIn: React.FC = () => {
-
   const { updateUser, /* other user data */ } = useUser();
 
-
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<UserData>({
     username: "",
     email: "",
     phone: "",
@@ -26,19 +210,18 @@ const SignIn: React.FC = () => {
     status: "offline",
   });
 
-
-
   const generateCaptcha = () => {
-    const captcha = crypto.randomBytes(3).toString("hex"); // Generate a n-character random string
+    const captcha = crypto.randomBytes(3).toString("hex");
     setFormData({ ...formData, captcha, enteredCaptcha: "" });
   };
 
   const handleRefreshCaptcha = () => {
-    generateCaptcha(); // Regenerate captcha when the button is clicked
+    generateCaptcha();
   };
 
-  const [isLoginMode, setIsLoginMode] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -47,28 +230,11 @@ const SignIn: React.FC = () => {
     });
   };
 
-  const handleSuccessfulLogin = () => {
+  const handleSuccessfulLogin = (username: string) => {
     setIsLoggedIn(true);
-    setFormData((prevData) => ({ ...prevData, status: "online" })); // Update status to "online"
-    updateUser({ ...formData});
-  };
-
-  const checkUniqueEmail = async (email: string) => {
-    const emailQuery = query(
-      collection(db, "userData"),
-      where("email", "==", email)
-    );
-    const emailSnapshot = await getDocs(emailQuery);
-    return emailSnapshot.empty;
-  };
-
-  const checkUniqueUsername = async (username: string) => {
-    const usernameQuery = query(
-      collection(db, "userData"),
-      where("username", "==", username)
-    );
-    const usernameSnapshot = await getDocs(usernameQuery);
-    return usernameSnapshot.empty;
+    setUsername(username);
+    setFormData((prevData) => ({ ...prevData, status: "online" }));
+    updateUser({ ...formData });
   };
 
   const handleSubmit = async () => {
@@ -77,97 +243,42 @@ const SignIn: React.FC = () => {
         throw new Error("Invalid captcha");
       }
 
-      // Check for unique username and email (if applicable)
-      if (!isLoginMode) {
-        const isUniqueUsername = await checkUniqueUsername(formData.username);
-        if (!isUniqueUsername) {
-          throw new Error(
-            "Username already exists. Please choose a different username or log in."
-          );
-        }
+      // Check if the user exists with the provided email
+      const userQuery = query(
+        collection(db, "userData"),
+        where("email", "==", formData.email)
+      );
+      const userSnapshot = await getDocs(userQuery);
 
-        const isUniqueEmail = await checkUniqueEmail(formData.email);
-        if (!isUniqueEmail) {
-          throw new Error("Email already exists. Please log in.");
-        }
+      if (userSnapshot.empty) {
+        throw new Error("Email not registered. Please register.");
       }
 
-      if (isLoginMode) {
-        // Check if the user exists with the provided email
-        const userQuery = query(
-          collection(db, "userData"),
-          where("email", "==", formData.email)
-        );
-        const userSnapshot = await getDocs(userQuery);
-
-        if (userSnapshot.empty) {
-          throw new Error("Invalid email or password");
-        }
-
-        // Check if the password matches
-        const userData = userSnapshot.docs[0].data();
-        if (userData.password !== formData.password) {
-          throw new Error("Invalid email or password");
-        }
-
-        console.log("Login successful!");
-        window.alert("Login successful!");
-        handleSuccessfulLogin();
-
-        // Redirect to the home page after showing the popup
-        // Replace the following line with the actual code to navigate to the home page
-        // For example, you can use React Router: history.push("/home");
-      } else {
-        // Registration logic
-        const existingUsername = await checkUniqueUsername(formData.username);
-        const existingEmail = await checkUniqueEmail(formData.email);
-
-        if (!existingUsername && !existingEmail) {
-          throw new Error("Username and Email already exist. Please log in.");
-        }
-
-        const existingUser = await createUserWithEmailAndPassword(
-          auth,
-          formData.email,
-          formData.password
-        );
-        const userId = existingUser.user?.uid;
-
-        const userDataRef = collection(db, "userData");
-        const userDocRef = doc(userDataRef, userId);
-
-        await setDoc(userDocRef, {
-          username: formData.username,
-          email: formData.email,
-          phone: formData.phone,
-          password: formData.password,
-          status: "online", // Set the initial status to "online" during registration
-        });
-
-        console.log("Registration successful!");
-        window.alert("Registration successful!");
+      // Check if the password matches
+      const userData = userSnapshot.docs[0].data() as UserData;
+      if (userData.password !== formData.password) {
+        throw new Error("Invalid password");
       }
+
+      console.log("Login successful!");
+      window.alert(`Welcome ${userData.username}! Login successful!`);
+      handleSuccessfulLogin(userData.username);
     } catch (error: any) {
-      console.error(
-        `Error during ${isLoginMode ? "login" : "registration"}:`,
-        error
-      );
-      window.alert(
-        `Error during ${isLoginMode ? "login" : "registration"}: ${
-          error.message
-        }`
-      );
+      console.error(`Error during login:`, error);
+      window.alert(`Error during login: ${error.message}`);
+      if (error.message === "Email not registered. Please register.") {
+        setIsLoginMode(false);
+      }
     } finally {
-      generateCaptcha(); // Regenerate captcha after each attempt
+      generateCaptcha();
     }
   };
 
   const toggleMode = () => {
     setIsLoginMode((prevMode) => !prevMode);
-    generateCaptcha(); // Regenerate captcha when toggling mode
+    generateCaptcha();
   };
 
-  // Initial captcha generation when component mounts
   React.useEffect(() => {
     generateCaptcha();
   }, []);
@@ -178,12 +289,11 @@ const SignIn: React.FC = () => {
         {isLoggedIn ? (
           <>
             <p className="text-xl font-semibold mb-4">
-              Welcome! You are logged in.
+              Welcome {username}! You are logged in.
             </p>
             <button
               onClick={() => (window.location.href = "/")}
               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-              // Add an onClick event to navigate to the home page or handle it according to your routing setup
             >
               Go to Home Page
             </button>
@@ -193,6 +303,16 @@ const SignIn: React.FC = () => {
             <h2 className="text-2xl font-semibold mb-4">
               {isLoginMode ? "Login" : "Registration"} Form
             </h2>
+            <label className="block mb-2">
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="block w-full mt-1 p-2 border rounded-md"
+              />
+            </label>
             {!isLoginMode && (
               <>
                 <label className="block mb-2">
@@ -217,16 +337,6 @@ const SignIn: React.FC = () => {
                 </label>
               </>
             )}
-            <label className="block mb-2">
-              Email:
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="block w-full mt-1 p-2 border rounded-md"
-              />
-            </label>
             <label className="block mb-2">
               Password:
               <input
