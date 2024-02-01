5f7cdf32cec154406d5c5497f6325556fe514e2a
Terminator1601 <rishisharma4201@gmail.com>
2024-02-01 14:43:04 +0530

    added css to popular search card

--- components/Cards/PopularSearchCard.tsx
+++ components/Cards/PopularSearchCard.tsx
@@ -11,11 +11,11 @@ interface PopularSearchCardProps {
   const PopularSearchCard: React.FC<PopularSearchCardProps> = ({ title, imageUrl }) => {
     return (
       <>
-        <div className="text-center border transition hover:scale-125 hover:delay-150">
+        <div className="text-center border border-amber-400	rounded-full transition hover:scale-125 hover:delay-150">
           <img
             src={imageUrl}
             alt={title}
-            className=" w-32 h-32 mx-auto d-block object-contain"
+            className=" w-32 h-32 mx-auto d-block object-contain rounded-full"
           />
           <h2 className='p-2 rounded-full ' style={{background: "var(--orange)" }}>{title}</h2>
           {/* <p>{description}</p> */}
--- components/Introduction/Intro.tsx
+++ components/Introduction/Intro.tsx
@@ -1,3 +1,60 @@
+// import React from "react";
+// import Link from "next/link";
+
+// const Intro = () => {
+//   return (
+//     <>
+//       <div className="place-content-center">
+//         <div className=" py-36 mx-5 grid grid-cols-2 text-center">
+//           <div className="text-center">
+//             <a href="/" className="hover:brightness-100 scale-150">
+//               <span style={{ color: "var(--orange)" }}>Ask</span>
+//               <span style={{ color: "var(--green)" }}>Your</span>
+//               <span style={{ color: "var(--blue)" }}>Dost</span>
+//             </a>
+//             <span>
+//               {" "}
+//               is a platform which provides you the information of the products
+//               and services which are provided in your locality.
+//             </span>
+//             <br />
+//             <br />
+//             <p>
+//               Here you can find information about the services available in your
+//               locality and you can also list your business services in this
+//               platform by simply clicking the{" "}
+//               <span className="hover:brightness-100 scale-150">
+//                 <a
+//                   href="/freeListing"
+//                   style={{ color: "var(--blue)" }}
+//                   className="bg-green"
+//                 >
+//                   FREELISTING
+//                 </a>
+//               </span>{" "}
+//               button and filling the details of your business.
+//             </p>
+//           </div>
+//           <div className="text-center">
+//             <img
+//               src="/images/intro-image.png"
+//               alt="intro image"
+//               className="w-56"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Intro;
+
+
+
+
+
+
 import React from "react";
 import Link from "next/link";
 
@@ -5,8 +62,8 @@ const Intro = () => {
   return (
     <>
       <div className="place-content-center">
-        <div className=" py-36 mx-5 grid grid-cols-2 text-center">
-          <div className="text-center">
+        <div className="py-36 mx-5 grid grid-cols-2 text-center">
+          <div className="text-center pt-16">
             <a href="/" className="hover:brightness-100 scale-150">
               <span style={{ color: "var(--orange)" }}>Ask</span>
               <span style={{ color: "var(--green)" }}>Your</span>
@@ -14,14 +71,14 @@ const Intro = () => {
             </a>
             <span>
               {" "}
-              is a platform which provides you the information of the products
-              and services which are provided in your locality.
+              is a platform that provides you with information about the products
+              and services available in your locality.
             </span>
             <br />
             <br />
             <p>
               Here you can find information about the services available in your
-              locality and you can also list your business services in this
+              locality, and you can also list your business services on this
               platform by simply clicking the{" "}
               <span className="hover:brightness-100 scale-150">
                 <a
@@ -32,14 +89,14 @@ const Intro = () => {
                   FREELISTING
                 </a>
               </span>{" "}
-              button and filling the details of your business.
+              button and filling in the details of your business.
             </p>
           </div>
-          <div className="text-center">
+          <div className="flex justify-center items-center">
             <img
               src="/images/intro-image.png"
               alt="intro image"
-              className="w-56"
+              className="w-72"
             />
           </div>
         </div>
