ca357ba9491009bffd66594eab9a334d3a77094e
Terminator1601 <rishisharma4201@gmail.com>
2024-06-05 15:44:42 +0530

    changed the localhost address

--- api/index.py
+++ api/index.py
@@ -240,4 +240,4 @@ if __name__ == '__main__':
     nltk.download('stopwords')
     nltk.download('punkt')
     nltk.download('wordnet')
-    app.run(debug=True, port=3000)
+    app.run(debug=True)
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -209,7 +209,7 @@ const Header: React.FC = () => {
 
     // Send the search term to the backend for prediction
     try {
-      const response = await fetch("http://127.0.0.1:3000/api/predict", {
+      const response = await fetch("http://127.0.0.1:5000/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
