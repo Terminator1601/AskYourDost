0d18658124e73a691772d72b3f1dcb098888dee0
Terminator1601 <rishisharma4201@gmail.com>
2024-10-02 19:16:23 +0530

    fixed header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -213,7 +213,7 @@ const Header: React.FC = () => {
 
     // Send the search term to the backend for prediction
     try {
-      const response = await fetch("http://localhost:3000/api/predict", {
+      const response = await fetch("https://ask-your-dost.vercel.app/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
